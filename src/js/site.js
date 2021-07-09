92ae00f337db56968114e5f25966c9a68b05c1d0
Willaam Rodrigues <williambairros@live.com>
2021-07-09 18:57:18 -0300

    Primeiro layout

---
+++ src/js/site.js
@@ -0,0 +1,43 @@
+/*!
+* Start Bootstrap - Simple Sidebar v6.0.2 (https://startbootstrap.com/template/simple-sidebar)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
+*/
+// 
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+         //Uncomment Below to persist sidebar toggle between refreshes
+         if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+             document.body.classList.toggle('sb-sidenav-toggled');
+         }
+         
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+});
+
+function setTogleText(){
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        if (sidebarToggle.textContent === "FECHAR MENU") {
+            sidebarToggle.textContent = "ABRIR MENU";
+        }
+        else {
+            sidebarToggle.textContent = "FECHAR MENU";
+        }
+    }
+}
+    
+
+
+
+
